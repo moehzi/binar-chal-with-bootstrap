38ab94391359e29b6a5383612f011d247962dfc0
moehzi <faizi1234098@gmail.com>
2021-10-03 08:57:56 +0800

    feat: Authorization with JWT

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
-const loginData = {
+const jwt = require("jsonwebtoken");
+
+const user = {
   username: "moehzi",
   password: "moehzi123",
 };
+
 app.use(express.json());
 
 app.use("/assets", express.static("public"));
@@ -21,20 +25,53 @@ app.get("/game", (req, res) => {
 
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
-  if (
-    loginData["username"] === username &&
-    loginData["password"] === password
-  ) {
-    res.status(201).send({
-      status: "201",
-      message: "Login success",
-    });
+  if (username === user.username && password === user.password) {
+    jwt.sign(
+      { username, password },
+      "secretkey",
+      { expiresIn: "30s" },
+      (err, token) => {
+        res.json({
+          token,
+        });
+      }
+    );
   } else {
-    res.status(401).send({
-      status: "401",
-      msg: "Wrong username or password",
-    });
+    res.sendStatus(401);
   }
 });
+
+app.post("/api/me", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(401);
+    } else {
+      res.json({
+        authData,
+      });
+      res.status(201);
+    }
+  });
+});
+
+// Format of Token
+// Authorization : Bearer <acess_token>
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    // Get Token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(401);
+  }
+}
 
 app.listen(5000);
